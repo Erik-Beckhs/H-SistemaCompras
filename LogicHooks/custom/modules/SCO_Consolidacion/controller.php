63f9198b2f5bf4e07da8e297c604a8022224ff27
Limberg Alcon <lifeaelg@gmail.com>
2020-06-26 15:42:32 -0400

    Controlador

---
+++ LogicHooks/custom/modules/SCO_Consolidacion/controller.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+*Esta clase realiza la accion de vista de creacion de consolidacion.
+*
+*@author Limberg Alcon <lalcon@hansa.com.bo>
+*@copyright 2020
+*@license /var/www/html/custom/modules/SCO_Consolidacion
+*/
+class SCO_ConsolidacionController extends SugarController {  
+
+  function action_creacion() {
+    $this-> view = 'creacion';
+  }
+  
+}
+?>
